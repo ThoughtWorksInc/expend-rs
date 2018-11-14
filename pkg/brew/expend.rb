4bd644fc35f768af1dd3bae1d40a5ee8d9748182
Sebastian Thiel <sthiel@thoughtworks.com>
2018-11-14 12:28:21 +0100

    Update brew to 1.0.1
    [skip CI]

--- pkg/brew/expend.rb
+++ pkg/brew/expend.rb
@@ -1,11 +1,11 @@
 class Expend < Formula
   # '---> DO NOT EDIT <--- (this file was generated from ./etc/brew/expend.rb.in'
-  version 'v1.0.0'
+  version '1.0.1'
   desc "Automate repetitive expenses for Expensify.com"
   homepage "https://github.com/Byron-TW/expend-rs"
 
-  url "https://github.com/Byron-TW/expend-rs/releases/download/v1.0.0/expend-v1.0.0-x86_64-apple-darwin.tar.gz"
-  sha256 "4e1dd6919ac530711ab1d48bea7c96d8072d2440e4b6d9bce836ba52d1d8446a"
+  url "https://github.com/Byron-TW/expend-rs/releases/download/1.0.1/expend-1.0.1-x86_64-apple-darwin.tar.gz"
+  sha256 "a7953295fe51e637d16835a8da5e8e10b9796a3af3fe4b523dc9d169a125b3ce"
 
   def install
     bin.install "expend"
